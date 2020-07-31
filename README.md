a157fe69cbc0110d229e1019a0266437f175e823
gino <gino@ginoah.com>
2020-07-31 11:20:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 中文轉注音、倉頡、拼音
+
+
+根據 [全字庫](https://data.gov.tw/dataset/5961) 做轉換
+
+```
+$ py example.py
+
+ch2en.zhuyin("測試")   : ㄘㄜˋㄕˋ
+ch2en.zhuen("測試")    : hk4g4
+ch2en.cangjie("測試")  : EBCNYRIPM
+ch2en.pinyin("測試")   : ce4shi4
+```
---
+++ example.py
@@ -0,0 +1,6 @@
+from ch2en import ch2en
+
+print('ch2en.zhuyin("測試")   :', ch2en.zhuyin('測試'))
+print('ch2en.zhuen("測試")    :', ch2en.zhuen('測試'))
+print('ch2en.cangjie("測試")  :', ch2en.cangjie('測試'))
+print('ch2en.pinyin("測試")   :', ch2en.pinyin('測試'))
